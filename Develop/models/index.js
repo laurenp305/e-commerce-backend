1d8b45d60966f8cbd4de1de6283fa8c6d1102ab9
Lauren Pineiro <lamor1800@gmail.com>
2022-10-04 20:45:31 -0400

    added product belongs to category in indexjs

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,10 +5,16 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
 
+
 // Products belongToMany Tags (through ProductTag)
+
+
 
 // Tags belongToMany Products (through ProductTag)
 
