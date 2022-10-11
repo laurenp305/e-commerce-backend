77727ff84f34d5559893d691f0b57e76f5dad96d
Lauren Pineiro <lamor1800@gmail.com>
2022-10-11 19:30:25 -0400

    added function to update category by id

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -62,6 +62,20 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  }).then((dbCategoryData) => {
+    if (!dbCategoryData) {
+      res.status(404).json({ message: 'No category with this id found. Try again.' });
+      return;
+    }
+    res.json(dbCategoryData)
+  }).catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
